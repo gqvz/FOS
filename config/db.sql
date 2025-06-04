0f98ade3c501dbb61f92fee2f8501c9a523ed6fe
gqvz <70444445+gqvz@users.noreply.github.com>
2025-06-05 00:08:49 +0530

    Initial commit

---
+++ config/db.sql
@@ -0,0 +1,98 @@
+CREATE TABLE `Users`
+(
+    `id`            INTEGER PRIMARY KEY,
+    `name`          VARCHAR(255)                     NOT NULL,
+    `email`         VARCHAR(255)                     NOT NULL,
+    `role`          ENUM ('customer','admin','chef') NOT NULL,
+    `password_hash` CHAR(60)                         NOT NULL
+);
+
+CREATE TABLE `Items`
+(
+    `id`           INTEGER PRIMARY KEY,
+    `name`         VARCHAR(32)   NOT NULL,
+    `description`  VARCHAR(255)  NOT NULL,
+    `price`        DECIMAL(6, 2) NOT NULL,
+    `is_available` BOOLEAN       NOT NULL,
+    `image_url`    VARCHAR(255)  NOT NULL
+);
+
+CREATE TABLE `Tags`
+(
+    `id`   INTEGER PRIMARY KEY,
+    `name` VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE `ItemTags`
+(
+    `item_id` INTEGER NOT NULL,
+    `tag_id`  INTEGER NOT NULL,
+    PRIMARY KEY (`item_id`, `tag_id`)
+);
+
+CREATE TABLE `Orders`
+(
+    `id`           INTEGER PRIMARY KEY,
+    `ordered_at`   DATETIME NOT NULL,
+    `customer_id`  INTEGER  NOT NULL,
+    `table_number` INTEGER  NOT NULL
+);
+
+CREATE TABLE `OrderItems`
+(
+    `id`                  INTEGER PRIMARY KEY          NOT NULL,
+    `order_id`            INTEGER                      NOT NULL,
+    `item_id`             INTEGER                      NOT NULL,
+    `custom_instructions` VARCHAR(255)                 NOT NULL,
+    `count`               INTEGER                      NOT NULL,
+    `status`              ENUM ('pending','completed') NOT NULL
+);
+
+CREATE TABLE `Sessions`
+(
+    `id`            INTEGER PRIMARY KEY,
+    `user_id`       INTEGER     NOT NULL,
+    `refresh_token` CHAR(32)    NOT NULL,
+    `name`          VARCHAR(32) NOT NULL,
+    `created_at`    DATETIME    NOT NULL,
+    `last_login`    DATETIME    NOT NULL
+);
+
+CREATE TABLE `Payments`
+(
+    `id`             INTEGER PRIMARY KEY,
+    `user_id`        INTEGER                         NOT NULL,
+    `cashier_id`     INTEGER                         NOT NULL,
+    `order_id`       INTEGER                         NOT NULL,
+    `order_subtotal` DECIMAL                         NOT NULL,
+    `tip`            DECIMAL(6, 2)                   NOT NULL,
+    `status`         ENUM ('processing','completed') NOT NULL,
+    `total`          DECIMAL(6, 2) GENERATED ALWAYS AS (order_subtotal + tip) STORED
+);
+
+ALTER TABLE `ItemTags`
+    ADD FOREIGN KEY (`item_id`) REFERENCES `Items` (`id`);
+
+ALTER TABLE `ItemTags`
+    ADD FOREIGN KEY (`tag_id`) REFERENCES `Tags` (`id`);
+
+ALTER TABLE `Orders`
+    ADD FOREIGN KEY (`customer_id`) REFERENCES `Users` (`id`);
+
+ALTER TABLE `OrderItems`
+    ADD FOREIGN KEY (`order_id`) REFERENCES `Orders` (`id`);
+
+ALTER TABLE `OrderItems`
+    ADD FOREIGN KEY (`item_id`) REFERENCES `Items` (`id`);
+
+ALTER TABLE `Sessions`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`);
+
+ALTER TABLE `Payments`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`);
+
+ALTER TABLE `Payments`
+    ADD FOREIGN KEY (`cashier_id`) REFERENCES `Users` (`id`);
+
+ALTER TABLE `Payments`
+    ADD FOREIGN KEY (`order_id`) REFERENCES `Orders` (`id`);
